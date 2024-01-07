c697096fd488c56dc37d79b85cff857166d3b0ee
machhammer <machhammer@gmx.net>
2024-01-07 14:19:07 +0100

    20240107

--- start_v2.sh
+++ start_v2.sh
@@ -5,4 +5,4 @@ source /home/pi/Projects/CryptoTrader/.venv/bin/activate
 python ma/finance/trader.py --coin=CQT --frequency=15m --live=true &
 python ma/finance/trader.py --coin=XRP --frequency=15m --live=true &
 python ma/finance/trader.py --coin=GMT --frequency=15m --live=true &
-python ma/finance/trader.py --coin=POWR --frequency=15m --live=true &
+python ma/finance/trader.py --coin=SOL --frequency=15m --live=true &
