43219c81be920e16fd239489f08d31cca9fa9f14
machhammer <machhammer@gmx.net>
2024-01-08 17:03:28 +0100

    20240108

--- ma/finance/manager.py
+++ ma/finance/manager.py
@@ -12,39 +12,24 @@ position_file = "positions.json"
 
 coins = {"XRP": 0.25, "XLM": 0.25, "GMT": 0.25, "SOL": 0.25}
 
-trader1 = TraderClass(
+traders = []
+threads = []
+
+for coin in coins:
+    traders.append(TraderClass(
     trading_mode="live",
-    coin="XLM",
-    coin_distribution={"XRP": 0.25, "FITFI": 0.25, "GMT": 0.25, "SOL": 0.25},
-    frequency="1m",
+    coin=coin,
+    coin_distribution=coins,
+    frequency="15m",
     exchange=exchange,
-)
+    ))
+    
 
-trader2 = TraderClass(
-    trading_mode="live",
-    coin="XRP",
-    coin_distribution={"XRP": 0.25, "FITFI": 0.25, "GMT": 0.25, "SOL": 0.25},
-    frequency="1m",
-    exchange=exchange,
-)
+for trader in traders:
+    threads.append(threading.Thread(target=trader.run, daemon=True))
 
-x = threading.Thread(target=trader1.run, daemon=True)
-y = threading.Thread(target=trader2.run, daemon=True)
+for thread in threads:
+    thread.start()
 
-x.start()
-y.start()
-
-x.join()
-y.join()
-
-print("wait")
-
-time.wait(15)
-
-print("stop")
-
-
-trader1.stop_running()
-trader2.stop_running()
-
-print("stopped")
+for thread in threads:
+    thread.join()
