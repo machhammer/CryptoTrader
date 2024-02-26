ec740d9b6c351b2ba93ffe8b74ea7f5ef7529e75
machhammer <machhammer@gmx.net>
2024-02-26 16:12:39 +0100

    20240226

--- ma/finance/models/V2.py
+++ ma/finance/models/V2.py
@@ -23,7 +23,7 @@ params = {
     "bb_dev": 2.5,
     "rsi_buy_threshold": 33,
     "rsi_sell_threshold": 73,
-    "profit_threshold": 0.6,
+    "profit_threshold": 0.8,
     "sell_threshold": 3,
     "urgency_sell": 3,
 }
