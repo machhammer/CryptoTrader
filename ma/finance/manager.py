e6d851275a532472aa905fc99c91da5ab9a1d876
machhammer <machhammer@gmx.net>
2024-03-08 12:00:23 +0100

    20240308

--- ma/finance/manager.py
+++ ma/finance/manager.py
@@ -225,6 +225,7 @@ def run():
             first_run = False
     
         if (datetime.now().minute >= 0 and datetime.now().minute <= 30 and datetime.now().hour == 1):
+            logger.info("Reset Daily Balance.")
             STOP_TRADING_FOR_TODAY = False  
             DAILY_STARTING_BALANCE == 0
 
@@ -323,10 +324,12 @@ def run():
                 logger.error("--- Error. Trader not alive!")
 
         if number_of_current_attempts_for_random_coins >= number_of_attempts_for_random_coins_wo_position:
+            logger.info("Reset number_of_current_attempts_for_random_coins = 1")
             number_of_current_attempts_for_random_coins = 1
 
         if has_random_coins_wo_position:
             number_of_current_attempts_for_random_coins = number_of_current_attempts_for_random_coins + 1
+            logger.info("Increase number_of_current_attempts_for_random_coins: {}".format(number_of_current_attempts_for_random_coins))
             has_random_coins_wo_position = False
 
         traders = traders_copy.copy()
