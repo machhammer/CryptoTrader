e53ff5ca7b9b76044d4dbba8724c49bf95bd27c2
machhammer <machhammer@gmx.net>
2024-08-01 18:58:13 +0200

    20240801

--- ma/finance/screener.py
+++ ma/finance/screener.py
@@ -245,7 +245,7 @@ def get_ticker_with_bigger_moves(exchange, tickers):
     progress_bar = iter(tqdm(range(iterations)))
     next(progress_bar)
     for ticker in tickers:
-        data = get_data(exchange, ticker, "5m", limit)
+        data = get_data(exchange, ticker, "1m", limit)
         #data["change"] = ((data["close"] - data["open"]) / data["open"]) * 100
         data["change"] = data["close"].pct_change()
         data["is_change_relevant"] = data["change"] >= move_increase_threshold
