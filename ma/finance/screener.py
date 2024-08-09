e746a89a9d8873f8bbe655c47d4ee763f091570c
machhammer <machhammer@gmx.net>
2024-08-09 16:49:56 +0200

    20240809

--- ma/finance/screener.py
+++ ma/finance/screener.py
@@ -27,7 +27,7 @@ logger.setLevel(logging.INFO)
 #************************************ Confiuguration
 exchange_name = "bitget"
 base_currency = "USDT"
-ignored_coins = [base_currency, "USDT", "USD", "CRO", "PAXG"]
+ignored_coins = [base_currency, "USDT", "USD", "CRO", "PAXG", "BGB"]
 amount_coins = 1000
 wait_time_next_asset_selection_minutes = 15
 wait_time_next_buy_selection_seconds = 60
