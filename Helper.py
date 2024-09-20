7b24b315bef7359c71878105741e43a6a222d39d
machhammer <machhammer@gmx.net>
2024-09-20 11:12:38 +0200

    20240920

--- Helper.py
+++ Helper.py
@@ -126,6 +126,6 @@ class Helper():
         database.insert_balance(self.get_time(), base_currency, balance)
     
     def read_last_balacne_from_db(self):
-        return database.execute_select("select balance from balance order by timestamp desc limit 1")
+        return database.execute_select("select balance from balance where DATE(`timestamp`) = CURDATE()-1 order by timestamp desc limit 1")
 
 
